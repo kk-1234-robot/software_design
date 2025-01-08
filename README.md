64d94bd8a987ebc55c86d1f091d5a9ecd3b85797
kkk_1234_robot <15979655914@163.com>
2025-01-08 15:32:22 +0800

    更新说明

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 软件使用说明
 
+## 配置运行软件
+
 ### Django
 
 请先配置 Django_projects/Software_design/Software_design/settings 文件中的MySQL的”DATABASES”和Django_projects/Software_design/my_app/llm-api中的“api-key”
@@ -7,7 +9,7 @@
 ```jsx
 cd Django_Projects
 cd Software_design
-//配置MYSQL数据库
+//配置MYSQL数据库中的表
 python python manage.py makemigrations my_app
 python manage.py migrate
 //运行
@@ -23,7 +25,40 @@ npm install
 npm run dev
 ```
 
+## 使用步骤
 
+### 首页
+
+在首页中也可以查看对应的使用说明
+
+### 上传标注任务
+
+- 点击左侧标注任务框，跳转到标注任务界面，输入标注任务名称以及轮次，最后将.csv文件拖入上传文件框中（或点击上传文件框再选择文件路径），上传文件。
+
+- 点击左侧标注框，跳转到标注界面。
+
+- 点击上一份或下一份按键切换标注的按键
+
+- 在案件文本框内修改案件后点击更新案件可以将案件信息更新
+
+- 点击上一句或下一句切换标注的句子
+
+- 开始标注
+
+  - 点击智能标注，可以将本句词语进行自动标注
+  - 点击上一个词或者下一个词可以查看本句中不同词语的标注情况，点击词性和实体按键可以看到其标注的词性和实体，勾选其下拉的选项后点击提交选择可以修改词性和实体标注情况
+
+- 查看结果：点击左侧结果框，跳转到结果界面可以查看所有已标注案件中的词性和实体分别情况
+
+- 保存结果：在结果界面点击导出数据可以将标注的数据导出
+
+  
+
+
+
+
+
+### 
 
 
 
--- vue_projects/src/components/Home_content.vue
+++ vue_projects/src/components/Home_content.vue
@@ -8,17 +8,15 @@
         :items="[
         {
           title: 'Step 1',
-          description: 'This is description 1 This is description 1 This is description 1 This is description 1 This is description 1 This is description 1 This is description 1' +
-           ' This is description 1 This is description 1 This is description 1 This is description 1 This is description 1 This is description 1 This is description 1 This is description 1 This is description 1' +
-            ' This is description 1 This is description 1 This is description 1 This is description 1 This is description 1 This is description 1',
+          description: '在标注任务页面中上传待标注的csv格式文件',
         },
         {
           title: 'Step 2',
-          description: 'This is description 2',
+          description: '在标注页面点击“下一份“选中文本，点击”下一句“选中文本中的句子，点击智能标注可以自动完成一个句子的标注。点击”下一个词“可以选中句子中的词，在下拉菜单中进行人工标',
         },
         {
           title: 'Step 3',
-          description: 'This is description 3',
+          description: '在结果页面中可以看到标注结果的图表，点击“导出结果”可以导出标注后的csv格式文件',
         },
       ]"
     ></a-steps>
